a048da80fdbb6e62da9b839bf00d94fcff834698
Shane <shanemcneil19@gmail.com>
2019-11-21 06:57:44 -0700

    prepare for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 export default {
-  API_ENDPOINT: `http://shane-bookmarks-server.herokuapp.com/api/bookmarks`,
+  API_ENDPOINT: `https://shane-bookmarks-server.herokuapp.com/api/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
-
-
- /*`https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`*/
